d848ed77eb06b593ecb9c300c9356e9398998a78
JabSysEmb <abduljabbar.sha97@gmail.com>
2024-02-10 16:54:28 +0300

    wip

--- src/routes/(authed)/profile/tasks/[taskID]/edit/+page.svelte
+++ src/routes/(authed)/profile/tasks/[taskID]/edit/+page.svelte
@@ -31,29 +31,4 @@
 		overflow: auto;
 		display: flex;
 	}
-
-	.info {
-		width: 100%;
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		grid-template-rows: repeat(auto-fit, minmax(0px, 1fr));
-	}
-
-	div.edit-icon {
-		position: absolute;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-inline-start: auto;
-		cursor: pointer;
-		flex-grow: 200px;
-		inset-block-end: 1rem;
-		inset-inline-end: 1rem;
-	}
-
-	@container balance (width < 550px) {
-		.info {
-			grid-template-columns: 1fr;
-		}
-	}
 </style>
